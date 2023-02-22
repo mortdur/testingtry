6750748ad9ef1e26e9de9f9a46cf36a091619f72
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-02-22 13:46:59 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -42,10 +42,19 @@ with col3:
 # LINK TO THE CSS FILE
 with open('.streamlit/style.css')as f:
  st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
-st.markdown("""
+hide="""
 <style>
-.viewerBadge_container__1QSob{visibility: hidden;}
+footer{
+	visibility: hidden;
+    position: relative;
+}
+.viewerBadge_container__1QSob{
+    visibility: hidden;
+}
+#MainMenu{
+	visibility: hidden;
+}
 <style>
-        """
+"""
 st.markdown(hide, unsafe_allow_html=True)
 
