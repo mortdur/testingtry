970723d31c540dddffd2a688b2fbb2926046f72e
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 21:38:29 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -55,6 +55,12 @@ def main():
             """
             , unsafe_allow_html=True
         )
+st.markdown("""
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+""", unsafe_allow_html=True)
+st.markdown("""
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+""", unsafe_allow_html=True)
 
         # Agregar contenido adicional
         st.header("Bienvenidos a mi página")
