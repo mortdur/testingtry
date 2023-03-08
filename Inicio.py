61c2cbaaa33b88ef46680d3b5cb12d1a21f2ca75
Miguel Ángel Ortega <121159231+mortdur@users.noreply.github.com>
2023-03-08 21:55:14 +0100

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -4,12 +4,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from PIL import Image
-from st_pages import show_pages_from_config
 from functions import *
-#PageConfig
-page_config = importar_config()
-#Menu
-show_pages_from_config()
 
 st.markdown("""
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -48,3 +43,6 @@ st.markdown("""
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 """, unsafe_allow_html=True)
+
+#CCS
+css = importar_css()
